fec5d1827c67bd34d72cd93176a9b03cc8355130
Minhvu Nguyen <manguye@ad.unc.edu>
2024-09-16 15:40:33 -0400

    Assignment_01

---
+++ assignment_01/hello_world.py
@@ -0,0 +1,25 @@
+'''
+This is
+a multiline
+comment
+'''
+
+"""
+Multi
+line
+comment
+"""
+
+# print on one line
+print('hello world')
+
+# print on many lines
+print(
+'''
+<p>this is a paragraph</p>
+
+
+
+<p>this is another paragraph</p>
+'''
+)
---
+++ assignment_01/idle.py
@@ -0,0 +1,18 @@
+'''
+Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+my_var = Minhvu
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    my_var = Minhvu
+NameError: name 'Minhvu' is not defined
+my_var = 'Minhvu'
+print(my_var)
+Minhvu
+my_var = 'Jax'
+print(my_var)
+Jax
+
+
+
+'''
